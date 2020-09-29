ea28ae377df23fdc9bea5a15981ffb0e9a99de02
Milan Gowda <46890041+MIlanJP@users.noreply.github.com>
2020-09-29 11:03:26 +0530

    test added test cases for counter element and increment element

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" data-test="appComponent">
+    <h1 data-test="counterDisplay"  >
+The counter is currently 
+    </h1>
+    <button data-test="incrementButton" >Increment Button</button>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,43 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Enzyme from 'enzyme'
-import EnzymeAdapter from 'enzyme-adapter-react-16'
-import App from './App';
-Enzyme.configure({adapter:new EnzymeAdapter()})
+import React from "react";
+import { render } from "@testing-library/react";
+import Enzyme, { shallow } from "enzyme";
+import EnzymeAdapter from "enzyme-adapter-react-16";
+import App from "./App";
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+const setup=(props={},state=null)=>{
+return shallow(<App {...props}/>)
+}
+
+const findByAttribute=(wrapper,testId)=>{
+return wrapper.find(`[data-test='${testId}']`)
+}
 
 
-test('renders learn react link', () => {
+test("renders without error", () => {
+  const wrapper=setup()
+  const appComponent=findByAttribute(wrapper,'appComponent')
+  expect(appComponent.length).toBe(1)
+});
 
+test("renders increment button", () => {
+    const wrapper=setup()
+    const appComponent=findByAttribute(wrapper,'appComponent')
+    expect(appComponent.length).toBe(1)
+});
+test("renders counters display", () => {
+    const wrapper=setup()
+  const button=findByAttribute(wrapper,'incrementButton')
+
+    expect(button.length).toBe(1)
+});
+test("counter starts at 0", () => {
+    const wrapper=setup()
+  const counterDisplay=findByAttribute(wrapper,'counterDisplay')
+    expect(counterDisplay.length).toBe(1)
+});
+test("renders learn react link", () => {
+    const wrapper=shallow(<App/>)
+    const appComponent=wrapper.find("[data-test='appComponent']");
+    expect(appComponent.length).toBe(1)
 });
